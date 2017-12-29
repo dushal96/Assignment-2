fc0a9f345d0e34dc49b3f7602747856b0d3cd8b5
Dushal Limbani <P15207862@LEC-ADMIN.dmu.ac.uk>
2017-12-29 03:53:56 +0000

    added api map also font and text added with colour changing mouseover text

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,13 +1,13 @@
-
-       function initMap() { 
-       var uluru = {lat: 52.630155, lng: -1.139180}; 
-        var map = new google.maps.Map(document.getElementById('map'), { 
-         zoom: 15, 
-          center: uluru 
-       }); 
-         var marker = new google.maps.Marker({ 
-          position: uluru, 
-           map: map 
-         }); 
-       } 
-
+function initMap() {
+   var uluru = {lat: 52.630155, lng: -1.139180};
+   var uluru = {lat: 52.630155, lng: -1.139180};
+   var map = new google.maps.Map(document.getElementById('map'), {
+   zoom: 15,
+     center: uluru
+   });
+   var marker = new google.maps.Marker({
+     position: uluru,
+     map: map
+   });
+}
+ 
